e1951ff9507ddaa09a4cf1e4e0ea38aae3671d16
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-30 18:25:10 +0100

    1. started coachpro project. 2. started main app

---
+++ coachpro/coachpro/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for coachpro project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coachpro.settings')
+
+application = get_wsgi_application()
