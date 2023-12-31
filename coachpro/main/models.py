d4921a3f82e75a2f699f5e948e1d33372e9fd43f
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-31 23:34:36 +0100

    1. created templates 2. created ClientListView + queryset with active clients

--- coachpro/main/models.py
+++ coachpro/main/models.py
@@ -6,3 +6,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_client = models.BooleanField(default=False)
     is_coach = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
+# class HomePageArticle(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField(max_length=1000)
+
+#     def __str__(self) -> str:
+#         return self.title
--- coachpro/main/views.py
+++ coachpro/main/views.py
@@ -1,3 +1,32 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.models import User
+
+from .models import Profile
+
 
 # Create your views here.
+# class HomeDetailView(DetailView):
+#     model = HomePageArticle
+#     template_name = "main/index.html"
+#     context_object_name = "article"
+
+
+def index(request):
+    return render(request, "main/index.html")
+
+
+class ClientListView(ListView):
+    model = Profile
+    template_name = "main/client_list.html"
+    context_object_name = "profiles"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return (
+            # Фильтруемся по Profile
+            Profile.objects.filter(is_client=True)
+            # Фильтруемся по полю Profile.user (которое имеет ключ и связано с User) и его свойству is_active
+            .filter(user__is_active=True)
+        )
