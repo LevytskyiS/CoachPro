422f18b163e64b03af3c25a842f9d2bc2ee46550
Serhii Levytskyi <levytskyi.sv@gmail.com>
2023-12-31 20:57:34 +0100

    created profile model

--- coachpro/main/models.py
+++ coachpro/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_client = models.BooleanField(default=False)
+    is_coach = models.BooleanField(default=False)
